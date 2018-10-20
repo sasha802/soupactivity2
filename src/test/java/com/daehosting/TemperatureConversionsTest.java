15cbf1536027382aae3e505ebd70cfd1e6d7c274
Sasha <oleksanova@gmail.com>
2018-10-19 19:11:19 -0500

    Adding activity 2 week8.

---
+++ src/test/java/com/daehosting/TemperatureConversionsTest.java
@@ -0,0 +1,41 @@
+package com.daehosting;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConversionsTest {
+
+    private TemperatureConversionsSoapType temperatureConversions = new TemperatureConversions().getTemperatureConversionsSoap12();
+
+    @Test
+    void getTemperatureConversionsSoap() {
+
+        BigDecimal result = temperatureConversions.fahrenheitToCelsius(BigDecimal.valueOf(32));
+        assertEquals(BigDecimal.valueOf(0), result);
+
+    }
+
+    @Test
+    void getTemperatureConversionsSoap1() {
+
+        BigDecimal result = temperatureConversions.windChillInCelsius(BigDecimal.valueOf(10), BigDecimal.valueOf(0));
+        assertEquals(BigDecimal.valueOf(11.21), result);
+    }
+
+    @Test
+    public void getTemperatureConversionsSoap12() throws Exception {
+
+        BigDecimal result = temperatureConversions.celsiusToFahrenheit(BigDecimal.valueOf(0));
+        assertEquals(BigDecimal.valueOf(32), result);
+    }
+
+    @Test
+    void getTemperatureConversionsSoap121() {
+
+        BigDecimal result = temperatureConversions.windChillInFahrenheit(BigDecimal.valueOf(45), BigDecimal.valueOf(20));
+        assertEquals(BigDecimal.valueOf(19.184), result);
+    }
+}
---
+++ src/test/java/com/daehosting/WindChillInCelsiusTest.java
@@ -0,0 +1,12 @@
+package com.daehosting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WindChillInCelsiusTest {
+
+    @Test
+    void getNCelsius() {
+    }
+}
